b96d8705f8c1877922cff31686eb774fd24144be
Kaustav Basu <kaustavbasu97@gmail.com>
2023-07-31 23:00:53 +0530

    conflict merged 2

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 def add(a,b) -> (int, int):
     return a+b
 =======
-def add(a,b,c) -> (int, int, int):
-    return a+b+c;
+def add(a,b,d) -> (int, int, int):
+    return a+b+d;
 >>>>>>> 20c7c5c12a599fb3a71f96d378777dd69843a0c1
 
 def sub(a,b, c) -> (int, int, int):
