d4e89b5aff5dade55910b4cd4cd6f217369bd64e
Kaustav Basu <kaustavbasu97@gmail.com>
2023-07-31 22:55:59 +0530

    sub updated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def add(a,b) -> (int, int):
-    return a+b;
+    return a+b
 
-def sub(a,b) -> (int, int):
-    return a-b
+def sub(a,b, c) -> (int, int, int):
+    return a-b-c
 
 def mul(a,b) -> (int, int):
     return a*b
